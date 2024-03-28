87bc7928f0f8aa7af198d778fcbc9df03ed59260
Lucas Pena <lucaspena4@gmail.com>
2024-03-28 20:19:19 -0300

    Sample Text

---
+++ SocialMediaApp/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="m-8" :class="{'dark': darkMode}">
+    <h1 class="font-mono">Sample Text</h1>
+  </div>
+</template>
+<script>
+
+  const darkMode = ref(false) 
+
+</script>
